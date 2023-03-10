2639624c96c132d1adca9969b8f938c271c01e5b
yonggyo1125 <yonggyo00@kakao.com>
2023-03-10 20:26:15 +0900

    .:

--- day07/src/main/java/org/koreait/config/ControllerConfig.java
+++ day07/src/main/java/org/koreait/config/ControllerConfig.java
@@ -1,5 +1,10 @@
 package org.koreait.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"org.koreait.controllers"})
 public class ControllerConfig {
 
 }
---
+++ day07/src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+${name}, 안녕하세요!
