df99b607c1a64a33e007b7063f5ad4952ed7c7d0
sonsungjong <tjdwhd70599@gmail.com>
2023-03-15 19:19:17 +0900

    .

--- day07/src/test/java/org/koreait/models/member/UserJoinService.java
+++ day07/src/test/java/org/koreait/models/member/UserJoinService.java
@@ -1,22 +1,19 @@
 package org.koreait.models.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserJoinService {
 	
 	private MemberDao memberDao;
 	
 	public boolean join(Member member) {
-		if (member == null) {
-			throw new UserJoinFailException("잘못된 접근입니다.");
-		}
 		
-		
-		// member  null 값이 아니면 db 처리, MemberDao::insert
-		int cnt = memberDao.insert(member);
-		
-		// 회원 가입 성공시
-		return cnt > 0;
+		return true;
 	}
 	
+	@Autowired
 	public void setMemberDao(MemberDao memberDao) {
 		this.memberDao = memberDao;
 	}
--- day07/src/test/java/org/koreait/tests/UserJoinTest.java
+++ day07/src/test/java/org/koreait/tests/UserJoinTest.java
@@ -1,69 +1,36 @@
 package org.koreait.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.koreait.controllers.users.MemberJoin;
+import org.koreait.config.ControllerConfig;
+import org.koreait.config.MvcConfig;
 import org.koreait.models.member.Member;
 import org.koreait.models.member.MemberDao;
-import org.koreait.models.member.UserJoinFailException;
 import org.koreait.models.member.UserJoinService;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@ExtendWith(MockitoExtension.class)
+
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@ContextConfiguration(classes= {MvcConfig.class, ControllerConfig.class})
+@WebAppConfiguration
 class UserJoinTest {
-	
-	@Mock
-	public MemberDao memberDao;
+		
+	@Autowired
+	private UserJoinService userJoinService;
 	
 	@Test
-	@DisplayName("회원가입 성공하면 true값을 반환")
-	void joinSuccessTest() {
+	@DisplayName("회원가입 성공시 true 반환")
+	public void joinSuccessTest() {
 		Member member = new Member();
-		member.setUserId("user01");
-		member.setUserPw("123456");
-		member.setUserNm("사용자01");
-		member.setEmail("user01@test.org");
-		member.setMobile("01000000000");
-		given(memberDao.insert(member)).willReturn(1);
+		boolean result = userJoinService.join(member);
 		
-		UserJoinService service = new UserJoinService();
-		service.setMemberDao(memberDao);
-		
-		boolean result = service.join(member);
-		 
 		assertEquals(result, true);
-	}
-	
-	
-	@Test
-	@DisplayName("MemberJoin 객체가 null이면 UserJoinFailException")
-	void joinValidationTest1() {
-		
-		UserJoinService service = new UserJoinService();
-		UserJoinFailException thrown = assertThrows(UserJoinFailException.class, () -> {
-			service.join(null);
-		});
-		
-		String message = thrown.getMessage();
-		assertEquals(message.contains("잘못된 접근입니다."), true);
-	}
-	
-	@Test
-	@DisplayName("Member userPw 비번이 해시가 안되어 있는 경우는 예외발생")
-	void joinValidationTest2() {
-		Member member = new Member();
-		member.setUserId("user01");
-		member.setUserPw("123456"); // 해시가 안되어 있는 비번 -> 예외발생 
-		
-		UserJoinService service = new UserJoinService();
-		assertThrows(UserJoinFailException.class, () -> {
-			service.join(member);
-		});
-		
 	}
 }
